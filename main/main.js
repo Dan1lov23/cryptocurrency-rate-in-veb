e630955b2ae6ce5625f5489a23920980d460366a
kitik <svy1dan@gmail.com>
2025-02-16 19:45:46 +0300

    add changes and split all my cod on а separate function.

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
 async function getBitcoinPrice() {
+    // Секция кода с биткоином
     const bitcoinName = 'bitcoin';
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=rub');
@@ -18,6 +19,7 @@ async function getBitcoinPrice() {
 getBitcoinPrice();
 
 async function getEthereumPrice() {
+    // Секция кода с эфириумом
     const ethereumName = 'ethereum';
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=rub');
@@ -37,6 +39,7 @@ async function getEthereumPrice() {
 getEthereumPrice();
 
 async function getAltcoinPrice() {
+    // Секция кода с альткоином
     const altcoinName = 'altcoin';
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=altcoin&vs_currencies=rub');
@@ -55,7 +58,8 @@ async function getAltcoinPrice() {
 
 getAltcoinPrice();
 
-async function getCardanoCost() {
+async function getCardanoPrice() {
+    // Секция кода с кардано
     const cardanoName = 'cardano';
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=rub');
@@ -72,5 +76,64 @@ async function getCardanoCost() {
     }
 }
 
-getCardanoCost();
+getCardanoPrice();
 
+
+async function getdashPrice() {
+    // Секция кода с dash
+    const dashName = 'dash';
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=dash&vs_currencies=rub');
+        if (!response.ok) {
+            throw new Error('Сеть не в порядке, не удалось получить данные');
+        }
+        const data = await response.json();
+        const dashPrice = data.dash.rub;
+        document.getElementById('dashLogo').innerHTML = `<img src="../img/coins.png"/>`;
+        document.getElementById('dash').innerHTML = `${dashPrice}₽`;
+        document.getElementById('dashName').innerText = `${dashName}`;
+    } catch (error) {
+        console.log('Ошибка', error);
+    }
+}
+
+getdashPrice();
+
+async function getLitecoinPrice () {
+    // Секция кода с лайткоином
+    const litecoinName = 'litecoin';
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=rub');
+        if (!response.ok) {
+            throw new Error('Сеть не в порядке, не удалось получить данные');
+        }
+        const data = await response.json();
+        const litecoinPrice = data.litecoin.rub;
+        document.getElementById('litecoinLogo').innerHTML = `<img src="../img/litecoin.png"/>`;
+        document.getElementById('litecoin').innerHTML = `${litecoinPrice}₽`;
+        document.getElementById('litecoinName').innerText = `${litecoinName}`;
+    } catch (error) {
+        console.log('Ошибка', error);
+    }
+}
+
+getLitecoinPrice();
+
+async function getTetherPrice() {
+    // Секция кода с tether
+    const tetherName = 'tether';
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=rub');
+        if (!response.ok) {
+            throw new Error('Сеть не в порядке, не удалось получить данные');
+        }
+        const data = await response.json();
+        const tetherPrice = data.tether.rub;
+        document.getElementById('tetherLogo').innerHTML = `<img src="../img/litecoin.png"/>`;
+        document.getElementById('tether').innerHTML = `${tetherPrice}₽`;
+        document.getElementById('tetherName').innerText = `${tetherName}`;
+    } catch (error) {
+        console.log('Ошибка', error);
+    }
+}
+getTetherPrice();
